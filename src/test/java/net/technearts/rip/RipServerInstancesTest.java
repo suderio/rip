8beca14f09f11cf580e96de398028fba337d9974
psude <paulo.suderio@bndes.gov.br>
2018-06-12 10:23:33 -0300

    Correção do teste

--- src/test/java/net/technearts/rip/RipServerInstancesTest.java
+++ src/test/java/net/technearts/rip/RipServerInstancesTest.java
@@ -2,9 +2,11 @@ package net.technearts.rip;
 
 import static io.restassured.RestAssured.when;
 import static net.technearts.rip.RipServer.localhost;
+import static net.technearts.rip.RipServer.stop;
 import static org.apache.http.HttpStatus.SC_NOT_FOUND;
 import static org.hamcrest.CoreMatchers.containsString;
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -13,16 +15,21 @@ import io.restassured.RestAssured;
 public class RipServerInstancesTest {
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
-        RestAssured.port = 7777;
+        RestAssured.port = 9999;
     }
-    
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        stop(9999);
+        stop(8888);
+    }
 	@Test
 	public void resetTest() throws InterruptedException {
-		localhost().get("/test").respond("Ok");
+		localhost(9999).get("/test").respond("Ok");
 		when().get("/test").then().content(containsString("Ok"));
-		localhost().reset();
-		localhost().get("/test2").respond("Ok");
+		localhost(9999).reset();
+		localhost(9999).get("/test2").respond("Ok");
 		// TODO TRATAR ERRO ABAIXO
+		localhost(8888).get("/").respond("");
 		localhost(8888).reset();
 		when().get("/test2").then().content(containsString("Ok"));
 		when().get("/test").then().statusCode(SC_NOT_FOUND);
