e2bc9483f2417925eae8cc5d9a23c4fcdaf2423d
Paulo Suderio <paulo.suderio@gmail.com>
2018-07-18 03:26:28 +0000

    Creating site for 0.0.7

--- pt/clover/net/technearts/rip/RipExcelReaderTest.js
+++ pt/clover/net/technearts/rip/RipExcelReaderTest.js
@@ -4,7 +4,7 @@ var clover = new Object();
 clover.pageData = {"classes":[{"el":28,"id":474,"methods":[{"el":16,"sc":3,"sl":13},{"el":26,"sc":3,"sl":18}],"name":"RipExcelReaderTest","sl":9}]}
 
 // JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
-clover.testTargets = {"test_0":{"methods":[{"sl":18}],"name":"test","pass":true,"statements":[{"sl":20},{"sl":21}]}}
+clover.testTargets = {"test_2":{"methods":[{"sl":18}],"name":"test","pass":true,"statements":[{"sl":20},{"sl":21}]}}
 
 // JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
-clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [0], [], [0], [0], [], [], [], [], [], [], []]
+clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [2], [], [2], [2], [], [], [], [], [], [], []]
